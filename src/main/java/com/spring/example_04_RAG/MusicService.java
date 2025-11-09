ce0cac9e3e3d2d7d440a5273ba76154e2b05e5b5
Michael Isvy <michael.isvy@gmail.com>
2025-11-09 22:17:15 +0800

    added example with apache tika reader

---
+++ src/main/java/com/spring/example_04_RAG/MusicService.java
@@ -0,0 +1,55 @@
+package com.spring.example_04_RAG;
+
+import dev.langchain4j.model.chat.ChatModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
+@Service
+class MusicService {
+
+    private static final Logger logger = LoggerFactory.getLogger(MusicService.class);
+
+
+    @Value("classpath:example_04_RAG/rock-albums.xlsx")
+    // replace it with @Value("classpath:music/rock-albums.pdf") in order to test reading from a PDF file
+    private Resource rockAlbumsResource;
+
+    private ChatModel chatModel;
+
+    public MusicService(ChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
+    private String loadDocumentText() {
+        try {
+            BufferedReader reader = new BufferedReader(
+                new InputStreamReader(this.rockAlbumsResource.getInputStream(), StandardCharsets.UTF_8)
+            );
+            return reader.lines().collect(Collectors.joining("\n"));
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load document", e);
+        }
+    }
+
+    public String findAlbumsFromArtist() {
+        var text = loadDocumentText();
+        logger.info(text);
+        
+        String prompt = String.format("""
+                Use the following pieces of context to answer the question at the end.
+                %s
+                Question: Which are the albums from Foo Fighters?
+                """, text);
+        
+        return this.chatModel.chat(prompt);
+    }
+}
---
+++ src/test/java/com/spring/example_04_RAG/MusicServiceTest.java
@@ -0,0 +1,22 @@
+package com.spring.example_04_RAG;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicServiceTest {
+
+    private final Logger logger = LoggerFactory.getLogger(MusicServiceTest.class);
+
+    @Autowired
+    private MusicService musicService;
+
+    @Test
+    void shouldFindSongsFromArtist() {
+        var response = this.musicService.findAlbumsFromArtist();
+        this.logger.info(response);
+    }
+}
